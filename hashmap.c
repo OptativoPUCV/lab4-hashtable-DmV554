233bbc14d7ee17610eb8210931afe39e8f87e852
DmV554 <127313442+DmV554@users.noreply.github.com>
2024-04-11 14:48:39 +0000

    new try

--- hashmap.c
+++ hashmap.c
@@ -129,7 +129,7 @@ Pair *firstMap(HashMap *map) {
 }
 
 Pair *nextMap(HashMap *map) { 
-  for (int i = map->current + 1; i < map->capacity; i++) {
+  for (int i = map->current; i < map->capacity; i++) {
     if (map->buckets[i] != NULL) {
         map->current = i; 
         return map->buckets[i];
